f5c63e00bd73ee15593b15bba625d66cad32d4c7
Sean Murphy <sean@iamseanmurphy.com>
2024-11-28 18:19:29 +0000

    2.1.3

---
+++ riovizual/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => 'bdaa8bfc69e174f5c1e9');
